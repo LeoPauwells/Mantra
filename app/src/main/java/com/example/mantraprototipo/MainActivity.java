2a798fb4b74c14294b3874c088a10f411714d80b
Francisco-Topete <71350426+Francisco-Topete@users.noreply.github.com>
2022-05-01 01:18:45 -0700

    Pantalla nueva

--- app/src/main/java/com/example/mantraprototipo/MainActivity.java
+++ app/src/main/java/com/example/mantraprototipo/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.learn_activity_login);
     }
 
     public void checkCurrentUser() {
