bc68d5183de8903be50755b4978f34e21ed315a5
LeoPauwells <leo.pauwells17@tectijuana.edu.mx>
2022-05-02 23:27:00 -0700

    quizz
    webview

--- app/src/main/java/com/example/mantraprototipo/Quizz_Activity.java
+++ app/src/main/java/com/example/mantraprototipo/Quizz_Activity.java
@@ -3,6 +3,7 @@ package com.example.mantraprototipo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Quizz_Activity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class Quizz_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizz);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://www.psicologia-online.com/escala-de-estabilidad-emocional-3667.html");
     }
 }
